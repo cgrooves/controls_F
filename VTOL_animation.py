cfb7e958b41c4296138403def557c8a7583539e5
cgrooves <caleb.j.groves@gmail.com>
2017-09-18 12:48:38 -0600

    Initial draft (untested) of drawing functions for VTOL

--- VTOL_animation.py
+++ VTOL_animation.py
@@ -4,6 +4,12 @@ import matplotlib.patches as mpatches
 import numpy as np
 import VTOL_params as P
 
+CENTER = 0
+LEFT_CONNECTOR = 1
+RIGHT_CONNECTOR = 2
+LEFT_ROTOR = 3
+RIGHT_ROTOR = 4
+
 class VTOL_animation:
 
     def __init__(self):
@@ -20,23 +26,69 @@ class VTOL_animation:
         h = u[1]
         theta = u[2]
 
-        self.body.move(u)
+        self.body.move(u) # translate and rotate skeleton
 
-        self.drawRotors(u)
-        self.drawCenter(u)
-        self.drawConnectors(u)
+        # draw out the updated components
+        self.drawRotors(theta)
+        self.drawCenter(theta)
+        self.drawConnectors()
 
         if self.flagInit == True:
             self.flagInit = False
 
-    def drawCenter(self,u):
+    def drawCenter(self,theta):
 
-    def drawConnectors(self,u):
+        xy = (self.body.keypoints[0,1],self.body.keypoints[1,1])
 
-    def drawRotors(self,u):
+        if self.flagInit == True:
+            self.handle.append(mpatches.Rectangle(xy,P.VTOL_center_size,\
+            P.VTOL_center_size,angle=u[2]))
+            self.ax.add_patch(self.handle[CENTER])
+        else:
+            self.handle[CENTER].set_xy(xy)
+
+    def drawConnectors(self):
+
+        x_left = (self.body.keypoints[0,0],self.body.keypoints[0,6])
+        y_left = (self.body.keypoints[1,0],self.body.keypoints[1,6])
+        x_right = (self.body.keypoints[0,2],self.body.keypoints[0,3])
+        y_right = (self.body.keypoints[1,2],self.body.keypoints[1,3])
+
+        if self.flagInit == True:
+            self.handle.append(mpl.lines.Line2D(x_left,y_left,lw=1,alpha=0.3))
+            self.handle.append(mpl.lines.Line2D(x_right,y_right,lw=1,alpha=0.3))
+            self.ax.add_line(self.handle[LEFT_CONNECTOR])
+            self.ax.add_line(self.handle[RIGHT_CONNECTOR])
+
+        else:
+            self.handle[LEFT_CONNECTOR].set_ydata(y_left)
+            self.handle[LEFT_CONNECTOR].set_xdata(x_left)
+            self.handle[RIGHT_CONNECTOR].set_ydata(y_right)
+            self.handle[RIGHT_CONNECTOR].set_xdata(x_right)
+
+    def drawRotors(self,theta):
 
         z = u[0]
         h = u[1]
         theta = u[2]
 
-        x =
+        x_left = self.body.keypoints[0,5]
+        y_left = self.body.keypoints[1,5]
+        x_right = self.body.keypoints[0,4]
+        y_right = self.body.keypoints[1,4]
+
+        xy_left = (x_left, y_left)
+        xy_right = (x_right, y_right)
+
+        if self.flagInit == True:
+            # Draw left rotor
+            self.handle.append(mpatches.Ellipse(xy_left,P.rotor_width,P.rotor_height\
+            ,angle=theta))
+            self.ax.add_patch(self.handle[LEFT_ROTOR])
+            # Draw right rotor
+            self.handle.append(mpatches.Ellipse(xy_right,P.rotor_width,P.rotor_height\
+            ,angle=theta))
+            self.ax.add_patch(self.handle[RIGHT_ROTOR])
+        else:
+            self.handle[LEFT_ROTOR].set_xy(xy_left)
+            self.handle[RIGHT_ROTOR].set_xy(xy_right)
